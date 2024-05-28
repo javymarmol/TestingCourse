87545764a90c59ee7bb4d81e642746694e23ba0f
Heyner Javier Marmol <contacto@javymarmol.com>
2024-05-27 23:32:44 -0500

    Writing first unit test

---
+++ app/src/test/java/com/plcoding/testingcourse/shopping/domain/ShoppingCartTest.kt
@@ -0,0 +1,53 @@
+package com.plcoding.testingcourse.shopping.domain
+
+import assertk.assertFailure
+import assertk.assertThat
+import assertk.assertions.isEqualTo
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+
+/**
+ * Created by Heyner Javier Marmol @javymarmol on 27/05/24.
+ * javymarmol.com
+ * Copyright (c) 2024 JavyMarmol. All rights reserved.
+ */
+internal class ShoppingCartTest {
+
+    private lateinit var cart: ShoppingCart
+
+    @BeforeEach
+    fun setUp() {
+        cart = ShoppingCart()
+    }
+
+    @Test
+    fun `Add multiple products, total price sum is correct`() {
+        // GIVEN
+        val product = Product(
+            id = 0,
+            name = "Ice cream",
+            price = 5.0
+        )
+        cart.addProduct(product, 4)
+
+        //ACTION
+        val priceSum = cart.getTotalCost()
+
+        //ASSERTION
+        assertThat(priceSum).isEqualTo(20.0)
+    }
+
+    @Test
+    fun `Ad product with negative quantity, throws Exception`() {
+        val product = Product(
+            id = 0,
+            name = "Ice cream",
+            price = 5.0
+        )
+
+        assertFailure {
+            cart.addProduct(product, -5)
+        }
+    }
+
+}
