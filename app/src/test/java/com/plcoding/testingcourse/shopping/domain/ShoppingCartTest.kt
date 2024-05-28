7d21f42204ea6d098455d6d1f87a4bb712be30e7
Heyner Javier Marmol <contacto@javymarmol.com>
2024-05-27 23:57:04 -0500

    How to test private functions?

--- app/src/test/java/com/plcoding/testingcourse/shopping/domain/ShoppingCartTest.kt
+++ app/src/test/java/com/plcoding/testingcourse/shopping/domain/ShoppingCartTest.kt
@@ -5,6 +5,7 @@ import assertk.assertThat
 import assertk.assertions.isEqualTo
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.RepeatedTest
+import org.junit.jupiter.api.Test
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.CsvSource
 
@@ -59,6 +60,21 @@ internal class ShoppingCartTest {
         assertFailure {
             cart.addProduct(product, -5)
         }
+    }
+
+    @Test
+    fun `isValidProduct returns invalid for not existing product`() {
+        val product = Product(
+            id = 1234,
+            name = "Ice cream",
+            price = 5.0
+        )
+
+        cart.addProduct(product, 4)
+
+        val totalPriceSum = cart.getTotalCost()
+
+        assertThat(totalPriceSum).isEqualTo(0.0)
     }
 
 }
