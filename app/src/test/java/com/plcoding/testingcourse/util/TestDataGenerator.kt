b86042394aebd33ccef4b0bff43243c47a543c92
Heyner Javier Marmol <contacto@javymarmol.com>
2024-07-10 17:25:41 -0500

    part7.3/Test launched coroutines

---
+++ app/src/test/java/com/plcoding/testingcourse/util/TestDataGenerator.kt
@@ -0,0 +1,32 @@
+package com.plcoding.testingcourse.util
+
+import com.plcoding.testingcourse.part7.domain.Post
+import com.plcoding.testingcourse.part7.domain.Profile
+import com.plcoding.testingcourse.part7.domain.User
+import java.util.UUID
+
+fun profile(): Profile {
+    val user = user()
+    return Profile(
+        user = user,
+        posts = (1..10).map {
+            post(userId = user.id)
+        }
+    )
+}
+
+fun post(userId: String): Post {
+    return Post(
+        id = UUID.randomUUID().toString(),
+        userId = userId,
+        title = "title post",
+        body = "body post"
+    )
+}
+
+fun user(): User {
+    return User(
+        id = UUID.randomUUID().toString(),
+        username = "test-user"
+    )
+}
